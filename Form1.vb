424e6caf1c57740f18cc3567fba210022e3d5a88
LonelyJonathan537 <adresonjohn8@gmail.com>
2024-06-12 12:54:28 +0800

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -1,4 +1,4 @@
-﻿Public Class Form1
+Public Class Form1
     Dim UsableChars() As String
     Dim RandomClass As New Random()
     Dim Charslist As String = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,1,2,3,4,5,6,7,8,9,0,!,@,#,$,%,^,&,*,(,),[,],?,`,~"
@@ -31,5 +31,20 @@
         Next
 
         txtPassword.Text = FinalPassword
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        SavePasswordToFile()
+    End Sub
+
+    Private Sub SavePasswordToFile()
+        Dim saveFileDialog As New SaveFileDialog()
+        saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+        saveFileDialog.Title = "Save Password"
+
+        If saveFileDialog.ShowDialog() = DialogResult.OK Then
+            Dim filePath As String = saveFileDialog.FileName
+            System.IO.File.WriteAllText(filePath, txtPassword.Text)
+        End If
     End Sub
 End Class
